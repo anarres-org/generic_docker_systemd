1ec3eff5a1a39d00d1c6035d1417dc5ea31633c7
m0wer <m0wer@autistici.org>
2018-07-02 13:24:43 +0200

    [test] Updated tests for db setup.
    Directories are not checked for now on as we don't create them.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,36 +6,29 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-@pytest.mark.parametrize("package", [
-    ("docker-ce"),
-    ("python-pip"),
-])
-def test_required_packages_exist(host, package):
-    pkg = host.package(package)
-    assert pkg.is_installed
+def test_dummy_service_conf(host):
+    service_conf = host.file("/etc/systemd/system/docker.dummy.service")
+    assert service_conf.exists
+    assert service_conf.contains("/bin/docker run")
+    assert service_conf.user == "root"
+    assert service_conf.group == "root"
 
 
-@pytest.mark.parametrize("pip_package", [
-    ("docker-py"),
-])
-def test_required_pip_packages_exist(host, pip_package):
-    pip_packages = host.pip_package.get_packages()
-    assert pip_package in pip_packages
+def test_dummy_db_service_conf(host):
+    service_conf_db = host.file("/etc/systemd/system/docker.dummy-db.service")
+    assert service_conf_db.exists
+    assert service_conf_db.contains("/bin/docker run")
+    assert service_conf_db.user == "root"
+    assert service_conf_db.group == "root"
 
 
-@pytest.mark.parametrize("directories", [
-    ("/root/docker/hello-world/data"),
-])
-def test_check_directories(host, directories):
-    with host.sudo():
-        directory = host.file(directories)
-        assert directory.exists
-        assert directory.user == 'root'
-        assert directory.group == 'root'
-        assert oct(directory.mode) == '0700'
+def test_dummy_db_service(host):
+    dummy_db = host.service("docker.dummy-db.service")
+    assert dummy_db.is_enabled
+    assert dummy_db.is_running
 
 
-def test_image_nginx_is_enabled_and_running(host):
-    service = host.service('hello-world')
-    assert service.is_enabled
-    assert service.is_running
+def test_dummy_service(host):
+    dummy = host.service("docker.dummy.service")
+    assert dummy.is_enabled
+    assert dummy.is_running
